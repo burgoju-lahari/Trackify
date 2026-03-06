56b9825d3fd471295978efac4bfc70beb809edc1
Lahari <22d21a1269lahari@gmail.com>
2026-03-07 02:34:39 +0530

    Added user authenctication and user-specific tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete = models. CASCADE)
     title = models.CharField(max_length = 200)    #title ->text field
     completed = models.BooleanField(default = False)   #completed -> true/false checkbox
     
--- tasks/views.py
+++ tasks/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def task_list(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         if title:
-            Task.objects.create(title=title)
+            Task.objects.create(
+                title=title,
+                user = request.user
+                )
 
         return redirect('task_list')
 
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user = request.user)
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
+@login_required
 def toggle_task(request, task_id):
-    task = Task.objects.get(id=task_id)
+    task = Task.objects.get(id=task_id, user= request.user) #user= request.user, so only users can touch their tasks
 
     if request.method == 'POST':
         task.completed = not task.completed
@@ -21,13 +27,15 @@ def toggle_task(request, task_id):
 
     return redirect('task_list')
 
+@login_required
 def delete_task(request, task_id):
-    task = Task.objects.get(id=task_id)
+    task = Task.objects.get(id=task_id, user= request.user)
     task.delete()
     return redirect('task_list')
 
+@login_required
 def edit_task(request, task_id):
-    task = Task.objects.get(id=task_id)
+    task = Task.objects.get(id=task_id, user= request.user)
 
     if request.method == 'POST':
         title = request.POST.get('title')
